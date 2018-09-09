5c78a8fff5ca1c837367023157b6b760040ac2ff
Gaurav <netgary7@gmail.com>
2018-09-09 06:32:21 +0000

    Adds functions exercises

---
+++ odd.js
@@ -0,0 +1,10 @@
+function isOdd (num){
+    if(num % 2 === 0){
+        return false;
+     } else  {
+        return true;
+     }
+
+}
+console.log("3 is Odd: "  + isOdd(3));
+console.log("8 is Odd: "  + isOdd(8));
