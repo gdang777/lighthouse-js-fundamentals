e0b4a92817029f233e1eea160ffe08180af830b2
Gaurav <netgary7@gmail.com>
2018-09-16 01:26:01 +0000

    Adds additional exercises

---
+++ go-outside.js
@@ -0,0 +1,17 @@
+var raining = true ;
+var cold = false ;
+
+if(raining) {
+    console.log("Dont forget your umbrella");
+
+}
+
+if (cold) {
+    console.log("Make sure to pick out a scarf!") ;
+
+} else {
+    console.log("Short sleeves are fine") ;
+}
+
+console.log("Now you are ready to go outside") ;
+
---
+++ go-outside2.js
@@ -0,0 +1,15 @@
+var temperature = -10 ;
+if( temperature < -20 || temperature > 30){
+    console.log("Maybe going outside isnt such a great idea") ;
+
+} else if (temperature < 0){
+    console.log("Make sure you pick out a scarf");
+
+ } else if (temperature < 15){
+    console.log("Short sleeves wont cut it!");
+ } else {
+    console.log("Short sleeves are fine!");
+ }
+
+ console.log("Now you are ready to go outside") ;
+
