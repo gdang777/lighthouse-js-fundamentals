79ddbe107f30c3cfb2ffb1cb3dac46e1a0cff92b
Gaurav <netgary7@gmail.com>
2018-09-16 04:15:13 +0000

    Adding ingredient exercise file - for and while loop

---
+++ ingredients.js
@@ -0,0 +1,16 @@
+var ingredients = ["eggs", "milk " , "flour" , "sugar", "baking soda" , "baking powder " , "chocolate chips" ,"bananas"]
+// writing the while loop
+// counter = 0 ;
+// while (counter < ingredients.length){
+//     console.log(ingredients[counter])
+//     counter++
+// }
+// writing the for loop
+// for (i = 0; i < ingredients.length; i++){
+//     console.log(ingredients[i]);
+// }
+
+// writing the loop to print the contents backwards
+for (i = ingredients.length - 1; i >= 0; i--){
+    console.log(ingredients[i]);
+}
